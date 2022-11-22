512aa51875bb9e6b39982d114687a9e6df3145a1
Ian Iversen <ianiversen1@gmail.com>
2022-11-22 15:38:45 -0500

    jsx done

--- JSX/jsx/src/App.js
+++ JSX/jsx/src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
 
 function App() {
+
+  return <p style={{
+    textAlign: 'center',
+    color: 'red',
+    fontSize: 44
+  }}>Hello World</p>
+
   // const error = true;
   // return <h1>{error ? 'Error' : 'Success'}</h1>
 
-  const props = {
-    id: 'input',
-    type: 'text',
-    maxLength: 3
-  }
 
-  return (
-    <>
-      <label htmlFor="input" >Input: </label>
-      <input {...props} placeholder="user" />
-    </>
-  );
+
+  // const props = {
+  //   id: 'input',
+  //   type: 'text',
+  //   maxLength: 3
+  // }
+
+  // return (
+  //   <>
+  //     <label htmlFor="input" >Input: </label>
+  //     <input {...props} placeholder="user" />
+  //   </>
+  // );
 }
 
 export default App;
