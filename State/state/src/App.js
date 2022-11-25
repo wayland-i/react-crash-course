4e0e406b5f5cb391d556bf8fcd7998bb97c1daaf
Ian Iversen <ianiversen1@gmail.com>
2022-11-24 20:02:38 -0500

    setter functions done

--- State/state/src/App.js
+++ State/state/src/App.js
@@ -3,16 +3,31 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(0);
   
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>
+      <Counter startingCount={10}></Counter>
+      <Counter></Counter>
+    </>
+  );
+}
+
+export default App;
+
+
+function Counter({startingCount = 0}) {
+  const [count, setCount] = useState(startingCount);
+  
+  return (
+    <>
+      <button onClick={() => {
+        setCount(prevCount => prevCount + 1);
+        setCount(prevCount => prevCount + 1);
+        setCount(prevCount => prevCount + 1);
+        }}>
         Increment
       </button>
       <p>Count: {count}</p>
     </>
   );
 }
-
-export default App;
