51bf2692922d28359c84106a204fdcb70f670b08
Ian Iversen <ianiversen1@gmail.com>
2022-11-24 23:36:35 -0500

    useRef focusing input

--- Refs/refs/src/App.js
+++ Refs/refs/src/App.js
@@ -2,24 +2,18 @@ import React, {useState, useRef} from 'react';
 
 
 function App() {
-  const [seconds, setSeconds] = useState(0);
-  const timerID = useRef(null);
-
-  const startTimer = () => {
-    timerID.current = setInterval(() => {
-      setSeconds(currSeconds => currSeconds + 1);
-    }, 1000);
-  };
-
-  const stopTimer = () => {
-    clearInterval(timerID.current);
-  };
   
+  const inputRef = useRef(null);
+
+  const focusInput = () => {
+    inputRef.current.focus();
+  }
+
+
   return (
     <>
-      <button onClick={startTimer}>Start</button>
-      <button onClick={stopTimer}>Stop</button>
-      <p>seconds: {seconds}</p>
+     <input ref={inputRef}/>
+     <button onClick={focusInput}>Focus</button>
     </>
   );
 }
