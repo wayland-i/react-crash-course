87c3fbdbc6371d9a6a512d15acc6063dbc87dead
Ian Iversen <ianiversen1@gmail.com>
2022-12-06 17:05:41 -0500

    custom hooks done

--- Writing Custom Hooks/writing-custom-hooks/src/App.js
+++ Writing Custom Hooks/writing-custom-hooks/src/App.js
@@ -4,29 +4,39 @@ export default function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState('');
 
-  const prevCount = useRef();
-  useEffect(() => {
-    prevCount.current = count;
-  }, [count]);
+  const prevCount = usePrevious(count);
+  const prevText = usePrevious(text);
 
-  const prevText = useRef();
-  useEffect(() => {
-    prevText.current = text;
-  }, [text]);
+  // const prevCount = useRef();
+  // useEffect(() => {
+  //   prevCount.current = count;
+  // }, [count]);
+
+  // const prevText = useRef();
+  // useEffect(() => {
+  //   prevText.current = text;
+  // }, [text]);
 
 
   return (
     <>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <p>Count: {count}</p>
-      <p>Previous render count: {prevCount.current}</p>
+      <p>Previous render count: {prevCount}</p>
 
       <input 
         value={text}
         onChange={(event) => setText(event.target.value)}/>
-      <p>Previous render text: {prevText.current}</p>
+      <p>Previous render text: {prevText}</p>
     </>
   );
+}
 
+function usePrevious(value) {
+  const prevRef = useRef();
+  useEffect(() => {
+    prevRef.current = value;
+  }, [value]);
 
+  return prevRef.current;
 }
