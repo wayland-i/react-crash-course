f6f6978e2af5352b77199660d2298afced15a1db
Ian Iversen <ianiversen1@gmail.com>
2022-12-07 17:46:27 -0500

    class based componenets done

--- Class-Based-Components/class-based-components/src/App.js
+++ Class-Based-Components/class-based-components/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import { createRef, Component, useState } from 'react';
+import { createRef, Component, useState, createContext } from 'react';
+
+const Theme = createContext({
+  mode: 'dark'
+})
 
 function App() {
   const [shouldRender, setShouldRender] = useState(true);
   return (
-    <>
+    <Theme.Provider value={{mode: 'dark'}}>
       <Counter startingCount={10}/>      
       { shouldRender && <Counter /> } 
       <button onClick={() => setShouldRender(!shouldRender)}>
@@ -12,7 +16,7 @@ function App() {
       </button>
       <div className='left'></div>
       <div className='right'></div>
-    </>
+    </Theme.Provider>
   );
 }
 
@@ -20,6 +24,7 @@ function App() {
 export default App;
 
 class Counter extends Component {
+  static contextType = Theme;
   // state = {
   //   count: this.props.startingCount ?? 0
   // }
@@ -35,6 +40,7 @@ class Counter extends Component {
   componentDidMount() {
     console.log('mounted');
     console.log(this.buttonRef);
+    console.log(this.context);
   }
 
   render() {
@@ -49,6 +55,12 @@ class Counter extends Component {
           Increment
         </button>
         <p>Count: {this.state.count}</p>
+        {/* <p>Theme: {this.context.mode}</p> */}
+        <Theme.Consumer>
+          {
+            context => <p>Theme: {context.mode}</p>
+          }
+        </Theme.Consumer>
       </>
     );
   }
