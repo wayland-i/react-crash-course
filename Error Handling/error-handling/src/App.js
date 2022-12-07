b2ad4ca2a2c2f85249289ece0ebca3f76e4534a7
Ian Iversen <ianiversen1@gmail.com>
2022-12-07 18:08:31 -0500

    errror fallback working

--- Error Handling/error-handling/src/App.js
+++ Error Handling/error-handling/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
-      <Buggy />
+      <ErrorBoundry fallback={<h1>There was an error</h1>}>
+        <Buggy />
+      </ErrorBoundry>
     </>
   );
 }
@@ -14,6 +16,21 @@ export default App;
 
 
 function Buggy() {
-  throw new Error('error');
+  // throw new Error('error');
   return <h1>Buggy</h1>;
+}
+
+class ErrorBoundry extends Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(error)  {
+    return { hasError: true, error };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback
+    }
+    return this.props.children
+  }
 }
